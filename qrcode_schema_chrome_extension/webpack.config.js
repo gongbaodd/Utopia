87e99d1b861d3da491791e64b1e411a58aa84f66
jian.gong <jian.gong@qunar.com>
2015-12-06 17:11:28 +0800

    init

--- qrcode_schema_chrome_extension/webpack.config.js
+++ qrcode_schema_chrome_extension/webpack.config.js
@@ -1,33 +1,15 @@
-var path = require("path");
-// For conveniance we create variable that holds the directory to bower_components
-var bower_dir = __dirname + '/bower_components';
-
 module.exports = {
-    entry: "./app/src/js/entry.js",
-    // The resolve.alias object takes require expressions
-    // (require('react')) as keys and filepath to actual
-    // module as values
-    resolve: {
-        alias: {
-            'react': bower_dir + '/react/react.min.js'
-        }
-    },
+    entry: './src/app.js',
     output: {
-        path: path.join(__dirname, "app/scripts"),
-        filename: "main.js"
+        filename: './prd/app.js'
     },
     module: {
-        // There is no reason for WebPack to parse this file
-        noParse: [bower_dir + '/react/react.min.js'],
-        loaders: [{
-                test: /\.css$/,
-                loader: "style!css"
-            },
-            // required for react jsx
-            {
-                test: /\.js$/,
-                loader: "jsx-loader"
-            }
-        ]
+        {
+            test: /\.js$/,
+            loader: 'babel-loader'
+        },{
+            test: /\.css$/,
+            loader: 'style-loader!css-loader'
+        }
     }
-};
+}
