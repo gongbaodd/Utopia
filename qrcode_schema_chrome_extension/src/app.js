d93077c562e72f6449d0d63a6c740d5d0d882def
jian.gong <jian.gong@qunar.com>
2015-12-08 22:17:28 +0800

    alpha2

--- qrcode_schema_chrome_extension/src/app.js
+++ qrcode_schema_chrome_extension/src/app.js
@@ -1,12 +1,9 @@
 import riot from 'riot';
 import './app.tag';
 
-// chrome.tabs.getSelected(null,()=>{
-    document.body.innerHTML += '<app></app>';
 
-    window.__APP__ = riot.mount('app')[0];
-
-// });
+document.body.innerHTML += '<app></app>';
+window.__APP__ = riot.mount('app')[0];
 
 import './router.js';
 import './style/global.scss';
--- qrcode_schema_chrome_extension/src/components/webview.js
+++ qrcode_schema_chrome_extension/src/components/webview.js
@@ -1,50 +1,56 @@
-const PAGE_URL = window.location.href;
 const QR_CODE = "http://qr.liantu.com/api.php?text=";
 
 import QRCode from 'qrcode';
 
 export function webvc(self,extra) {
     var root = self.root;
-    var href = PAGE_URL;
-    var host = '://web/url?url=';
-    var protocol = chooseOS(root);
-    var schema = [protocol,host,encodeURIComponent(href)].join('');
-    genrtateQR(self,schema);
+    getURL(function(href) {
+        var host = '://web/url?url=';
+        var protocol = chooseOS(root);
+        var schema = [protocol,host,encodeURIComponent(href)].join('');
+        genrtateQR(self,schema);
 
-    self.schema = schema;
+        self.schema = schema;
+    });
 };
 
 export function transparent(self,extra) {
     var root = self.root;
-    var href = PAGE_URL;
-    var host = '://hy/url?type=navibar-transparent&url=';
-    var protocol = chooseOS(root);
-    var schema = [protocol,host,encodeURIComponent(href)].join('');
-    genrtateQR(self,schema);
 
-    self.schema = schema;
+    getURL(function(href) {
+        var host = '://hy/url?type=navibar-transparent&url=';
+        var protocol = chooseOS(root);
+        var schema = [protocol,host,encodeURIComponent(href)].join('');
+        genrtateQR(self,schema);
+
+        self.schema = schema;
+    });
 };
 
 export function none(self,extra) {
     var root = self.root;
-    var href = PAGE_URL;
-    var host = '://hy/url?type=navibar-none&url=';
-    var protocol = chooseOS(root);
-    var schema = [protocol,host,encodeURIComponent(href)].join('');
-    genrtateQR(self,schema);
 
-    self.schema = schema;
+    getURL(function(href) {
+        var host = '://hy/url?type=navibar-none&url=';
+        var protocol = chooseOS(root);
+        var schema = [protocol,host,encodeURIComponent(href)].join('');
+        genrtateQR(self,schema);
+
+        self.schema = schema;
+    });
 };
 
 export function normal(self,extra) {
     var root = self.root;
-    var href = PAGE_URL;
-    var host = '://hy/url?url=';
-    var protocol = chooseOS(root);
-    var schema = [protocol,host,encodeURIComponent(href)].join('');
-    genrtateQR(self,schema);
 
-    self.schema = schema;
+    getURL(function(href) {
+        var host = '://hy/url?url=';
+        var protocol = chooseOS(root);
+        var schema = [protocol,host,encodeURIComponent(href)].join('');
+        genrtateQR(self,schema);
+
+        self.schema = schema;
+    });
 };
 
 function chooseOS(root) {
@@ -65,4 +71,10 @@ function genrtateQR(self,schema){
     var img = new QRCode({text: schema,size: 300});
 
     qrcode.update({qrcode: img});
-}
+};
+
+function getURL(callback) {
+    chrome.tabs.getSelected(null,(tab)=>{
+        callback(tab.url);
+    });
+};
